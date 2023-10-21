32a7d38f03e069e48068140957b099d622d4b748
Nitin <nitinmathe3010@gmail.com>
2023-10-21 22:37:11 +0530

    mergin mergeLoginpage

---
+++ src/main/java/basicTest/MergingLoginPage.java
@@ -0,0 +1,13 @@
+package basicTest;
+
+public class MergingLoginPage  {
+
+	public static void main(String[] args) {
+		
+		String s="abc";
+		
+		System.out.println("Printing S variable on MergingLoginPage : " + s);
+		
+	}
+	
+}
