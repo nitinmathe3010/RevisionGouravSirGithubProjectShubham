50cf50b263b657f433412138f04d6422909dcb83
Nitin <nitinmathe3010@gmail.com>
2023-10-21 20:15:54 +0530

    added loginpage on Nitin branch

---
+++ src/main/java/basicTest/Loginpage.java
@@ -0,0 +1,13 @@
+package basicTest;
+
+public class Loginpage{
+
+	public static void main(String[] args) {
+		
+		String s="abc";
+		
+		System.out.println("Printing S variable on Loginpage : " + s);
+		
+	}
+	
+}
